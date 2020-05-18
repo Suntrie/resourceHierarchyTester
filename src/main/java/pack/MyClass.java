e7c941f5233128fb5077047bda61cf097572b55e
is.egorova@mail.ru <Venarimlondon406*>
2020-05-18 15:36:52 +0300

    Initial commit

---
+++ src/main/java/pack/MyClass.java
@@ -0,0 +1,24 @@
+package pack;
+
+import java.io.*;
+
+public class MyClass {
+
+    public static void main(String[] args) throws IOException {
+
+        // Will work when start from working directory (when relative path exists)
+
+        //File newFile = new File("src/main/resources/myfile.txt");
+        //System.out.println(Files.readString(newFile.toPath()));
+
+        // Works when file inside jar (path is relative to the directory named as package for current class)
+        InputStream in = MyClass.class.getResourceAsStream("myfile.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        System.out.println(reader.readLine());
+
+        // Works when file inside jar (path is relative to the Resources directory)
+        InputStream in1 = MyClass.class.getResourceAsStream("/myfile1.txt");
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(in1));
+        System.out.println(reader1.readLine());
+    }
+}
